85a30fee5a5fc3f2e6652034b06128cd2e718e4f
Ab4rdo <fryderyk97@protonmail.com>
2017-11-26 16:16:56 +0100

    init insertion sort

---
+++ insertion_sort/InsertionSort.py
@@ -0,0 +1,13 @@
+
+
+def sort(seq):
+    """
+    Implementation of insertion sort algorithm
+    :param seq: a integer list
+    :return: new ascending sorted list
+    """
+
+    if not seq:
+        raise ValueError('The list is empty')
+
+
---
+++ insertion_sort/TestInsertionSort.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestInsertionSort(unittest.TestCase):
+    pass
