f0d65a3ea050c33225114bc238d45c3353932eee
Ab4rdo <fryderyk97@protonmail.com>
2017-11-26 15:27:12 +0100

    improved comment

--- binary_search/BinarySearch.py
+++ binary_search/BinarySearch.py
@@ -1,20 +1,20 @@
 
 
 def search(seq, target):
-        """
-        Implementation of binary_search search algorithm
-        seq - a int list
-        target - a value to search
-        return - index of target or raise ValueError if target not in the seq or if seq is empty
-        """
+    """
+    Implementation of binary_search search algorithm
+    :param seq: a integer list
+    :param target: a value to search
+    :return: index of target or raise ValueError if target not in the seq or if seq is empty
+    """
 
-        if not seq:
-            raise ValueError('List is empty or the target is not in the list')
+    if not seq:
+        raise ValueError('List is empty or the target is not in the list')
 
-        guess = int((len(seq))/2)
-        if target == seq[guess]:
-            return guess
-        if target > seq[guess]:
-            return guess + 1 + search(seq[guess+1:], target)
-        if target < seq[guess]:
-            return search(seq[:guess], target)
+    guess = int((len(seq))/2)
+    if target == seq[guess]:
+        return guess
+    if target > seq[guess]:
+        return guess + 1 + search(seq[guess+1:], target)
+    if target < seq[guess]:
+        return search(seq[:guess], target)
