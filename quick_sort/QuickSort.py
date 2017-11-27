ffea73a8499b2b16927d5af2e97a78340aaf4a5f
Ab4rdo <fryderyk97@protonmail.com>
2017-11-27 12:36:23 +0100

    Init Quick sort

---
+++ quick_sort/QuickSort.py
@@ -0,0 +1,17 @@
+
+
+def sort(seq):
+    """
+    Implementation of basic quick sort algorithm
+    :param seq: an integer list
+    :return: new sorted integer list
+    """
+
+    pass
+
+
+
+def partition():
+
+    pass
+
---
+++ quick_sort/TestQuickSort.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestQuickSort(unittest.TestCase):
+
+    def test_partition_1(self):
+        pass
+
+    def test_quick_sort_1(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
